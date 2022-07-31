b107754aee864f9a684de0162eea127365c145ef
ydzhony1 <ydzhony1@163.com>
2022-07-31 21:44:28 +0800

    update vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="g-index">
     <h2 class="tit">简版webpack+vue3 手动配置</h2>
+    <div class="txt-tit">{{ book }}</div>
     <div id="nav" v-if="!$route.meta.hide">
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
@@ -13,15 +14,43 @@
 </template>
 
 <script>
-import { defineComponent, computed } from 'vue'
+import { defineComponent, onMounted, computed, ref, reactive } from 'vue'
 import { useStore } from 'vuex'
-export default defineComponent({
+export default {
+  props: {
+    collectionName: String,
+  },
+  setup(props) {
+    const readersNumber = ref(0)
+    const book = reactive({ title: 'Vue 3 Guide' })
+    onMounted(() => {
+      console.log(book.title, 'this book')
+    })
+    // 暴露给 template
+    return {
+      readersNumber,
+      book,
+    }
+  },
+}
+/* export default defineComponent({
   setup() {
     const store = useStore()
+    const methods = {
+      countstore() {
+        console.log(555)
+      },
+    }
+
     return {
+      ...methods,
       title: computed(() => store.state.title),
     }
   },
 })
+console.log(333)
+onMounted(() => {
+  console.log(115522)
+}) */
 </script>
 <style lang="scss" scoped></style>
